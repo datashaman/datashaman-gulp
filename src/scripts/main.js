9fe2d958abb16bd6b397bf40f3588605d912eb75
datashaman <marlinf@datashaman.com>
2020-02-22 12:13:05 +0200

    fix format

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -17,6 +17,11 @@ ready(function() {
         el.innerText = time.toLocaleString('en-ZA', {
             dateStyle: 'short',
             timeStyle: 'short',
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
         })
     })
 })
